5215f7054c91ae4cacbb8d142464a1588ce9b54f
Илья Прокофьев <prokil1805@gmail.com>
2025-11-12 15:30:20 +0300

    gg

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.2s15uknblb"
+    "revision": "0.8aqjdnh20dg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
