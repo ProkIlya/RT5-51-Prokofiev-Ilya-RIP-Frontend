f360c56be9163cf4e7fdf5a534c2ff7df86dbae6
Илья Прокофьев <prokil1805@gmail.com>
2025-11-15 14:40:49 +0300

    Перевел Бэкенд на https

--- backend/internal/pkg/app.go
+++ backend/internal/pkg/app.go
@@ -24,7 +24,23 @@ func NewApp(c *config.Config, r *gin.Engine, h *handler.Handler) *Application {
 	}
 }
 
-func (a *Application) RunApp() {
+// CORS middleware
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+/*func (a *Application) RunApp() {
 	logrus.Info("Server start up")
 
 	a.Handler.RegisterHandler(a.Router)
@@ -32,6 +48,25 @@ func (a *Application) RunApp() {
 	serverAddress := fmt.Sprintf("%s:%d", a.Config.ServiceHost, a.Config.ServicePort)
 	if err := a.Router.Run(serverAddress); err != nil {
 		logrus.Fatal(err)
+	}
+	logrus.Info("Server down")
+}
+*/
+
+func (a *Application) RunApp() {
+	logrus.Info("Server start up")
+
+	// Добавляем CORS middleware
+	a.Router.Use(corsMiddleware())
+
+	a.Handler.RegisterHandler(a.Router)
+
+	serverAddress := fmt.Sprintf("%s:%d", a.Config.ServiceHost, a.Config.ServicePort)
+
+	// Запускаем HTTPS сервер
+	logrus.Info("Starting HTTPS server...")
+	if err := a.Router.RunTLS(serverAddress, "cert.crt", "cert.key"); err != nil {
+		logrus.Fatal("HTTPS server failed: ", err)
 	}
 	logrus.Info("Server down")
 }
--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.af7735vfkng"
+    "revision": "0.sg9j4hrf11g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
